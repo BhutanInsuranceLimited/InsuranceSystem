26cadf3551ae0b7e5516c1a4e4bd402cbc596869
jimmychoxiling <jeecee191@gmail.com>
2016-12-03 12:59:29 -0800

    all css

---
+++ storage/framework/views/8ea672f6d4130d18614c1b02f7c76d19cd957491.php
@@ -0,0 +1,74 @@
+<div class="page-sidebar-wrapper">
+    <div class="page-sidebar navbar-collapse collapse">
+
+        <div class="portlet light profile-sidebar-portlet margin-left-20">
+            <!-- SIDEBAR USERPIC -->
+            <div class="profile-userpic margin-top-20 margin-left-20">
+                <img src="<?php echo e(asset('public/admin/layout/img/avatar.png')); ?>" class="img-responsive" alt="User Image">
+            </div>
+            <!-- END SIDEBAR USERPIC -->
+        </div>
+        <ul class="page-sidebar-menu">
+            <li class="start active open">
+                <a href="javascript:;">
+                    <span class="title"> Profile </span>
+                    <span class="selected"></span>
+                    <span class="arrow open"></span>
+                </a>
+                <ul class="sub-menu">
+                    <li class="active">
+                        <a href=""> Identification </a>
+                    </li>
+                    <li>
+                        <a href=""> Contact </a>
+                    </li>
+                    <li>
+                        <a href=""> Work Details </a>
+                    </li>
+                    <li>
+                        <a href=""> Financials </a>
+                    </li>
+                    <li>
+                        <a href=""> Demographics </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <a href="#leads" data-toggle="tab" role="tab">
+                    <span class="title"> Leads </span>
+                </a>
+            </li>
+            <li>
+                <a href="#quotation" data-toggle="tab" role="tab">
+                    <span class="title"> Quotation </span>
+                </a>
+            </li>
+            <li>
+                <a href="#payments" data-toggle="tab" role="tab">
+                    <span class="title"> Payments </span>
+                </a>
+            </li>
+            <li>
+                <a href="#claim" data-toggle="tab" role="tab">
+                    <span class="title"> Claim </span>
+                </a>
+            </li>
+            <li>
+                <a href="#document" data-toggle="tab" role="tab">
+                    <span class="title"> Document </span>
+                </a>
+            </li>
+            <li>
+                <a href="javascript:;">
+                    <span class="title"> Service Request </span>
+                </a>
+            </li>
+            <li class="last">
+                <a href="javascript:;">
+                    <span class="title">  Complaint</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>

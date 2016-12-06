03820f5673d734182b2c94a96a589dc45f8bfc8f
jimmychoxiling <jeecee191@gmail.com>
2016-12-06 15:47:32 -0800

    Login commit

--- config/auth.php
+++ config/auth.php
@@ -102,7 +102,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => Namespace\Of\Your\User\Model\User::class,
+            'model' => App\User::class,
             'table' => 'users',
         ],
     ],
